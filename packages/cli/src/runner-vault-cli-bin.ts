a42472d38b239401eb1fcf7940723b679ad68ba0
rocketman <20303031+rocketman-21@users.noreply.github.com>
2026-04-13 09:20:37 +1000

    chore(repo): remove legacy internal wrapper shims

--- packages/cli/src/runner-vault-cli-bin.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-import { formatMurphCliError } from './cli-entry.js'
-import { runRunnerVaultCliEntrypoint } from './runner-vault-cli.js'
-
-runRunnerVaultCliEntrypoint().catch((error) => {
-  console.error(formatMurphCliError(error))
-  process.exitCode = 1
-})
